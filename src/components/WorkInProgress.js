6776beed0500c0a10a6d8dd385f5a41b8a978bed
EdinK1 <edinkaymakchi@gmail.com>
2020-01-08 20:03:06 -0800

    add work in progress bar

---
+++ src/components/WorkInProgress.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const WorkInProgress = () => <span className="danger">Work in progress</span>
+
+export default WorkInProgress
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
+import WorkInProgress from "./WorkInProgress"
 
 const Landing = () => (
   <section id="landing" className="container landing">
+    <WorkInProgress />
     <h1>Edin Kaymakqi</h1>
     <p className="landing-description">Web Developer living in Vancouver</p>
     <Link to="#showcase" className="btn">
